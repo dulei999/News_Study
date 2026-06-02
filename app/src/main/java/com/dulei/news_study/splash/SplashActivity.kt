6127844092c1b456d066ece0bd562eb59834ccf2
dulei <1548352188@qq.com>
2026-06-02 20:02:37 +0800

    first commit

---
+++ app/src/main/java/com/dulei/news_study/splash/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.dulei.news_study.splash
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dulei.news_study.R
+
+/**
+ * 启动界面
+ */
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
